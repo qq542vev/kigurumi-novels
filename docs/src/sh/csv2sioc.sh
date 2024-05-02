f12b8e3a5bf88311d6a059f6c568155cd5af3981
qq542vev <qq542vev@yahoo.co.jp>
2024-05-02 17:50:23 +0900

    コード内容をリファクタリング。

--- docs/src/sh/csv2sioc.sh
+++ docs/src/sh/csv2sioc.sh
@@ -6,8 +6,9 @@ awkScript=$(
 	cat <<-'__EOF__'
 	# @cuktash use-block begin
 	# @cuktash awk-use cuktash/csv/parse * csv_
-	# @cuktash awk-use cuktash/str/sanitize * str_
-	# @cuktash awk-use cuktash/xml/gen/element *
+	# @cuktash awk-use cuktash/xml/sanitize *
+	# @cuktash awk-use cuktash/xml/gen/elem *
+	# @cuktash awk-use cuktash/xml/gen/attr *
 	# @cuktash use-block end
 
 	BEGIN {
@@ -16,15 +17,15 @@ awkScript=$(
 		for(count = 1; (count, 1) in array; count++) {}
 		count--
 
-		board = element("dcterms:title", "", ARGV[2])
-		board = board element("sioc:num_items", "rdf:datatype=\"&xsd;nonNegativeInteger\"", count)
+		board = elem("dcterms:title", "", ARGV[2])
+		board = board elem("sioc:num_items", attr("rdf:datatype", "&xsd;nonNegativeInteger", 0), count)
 
 		if(1 <= count) {
-			board = board element("dcterms:created", "rdf:datatype=\"&dcterms;W3CDTF\"", array[1, 4])
-			board = board element("sioc:last_item_date", "rdf:datatype=\"&dcterms;W3CDTF\"", array[count, 4])
+			board = board elem("dcterms:created", attr("rdf:datatype", "&dcterms;W3CDTF", 0), array[1, 4])
+			board = board elem("sioc:last_item_date", attr("rdf:datatype", "&dcterms;W3CDTF", 0), array[count, 4])
 		}
 
-		printf("%s", element("types:MessageBoard", "rdf:about=\"&board;\"", board, 0))
+		printf("%s", elem("types:MessageBoard", attr("rdf:about", "&board;", 0), board, 0))
 
 		for(i = 1; (i, 1) in array; i++) {
 			number = array[i, 1]
@@ -33,25 +34,25 @@ awkScript=$(
 			date = array[i, 4]
 			content = array[i, 5]
 
-			post = element("rdfs:label", "rdf:datatype=\"&xsd;positiveInteger\"", number)
+			post = elem("rdfs:label", attr("rdf:datatype", "&xsd;positiveInteger", 0), number)
 
 			if(name != "" || trip != ""){
 				creator = ""
 
 				if(name != "") {
-					creator = creator element("foaf:nick", (name == "名無しさん@着ぐるみすと" ? "xml:lang=\"ja\"" : "rdf:datatype=\"&xsd;string\""), str_sanitize(name))
+					creator = creator elem("foaf:nick", (name == "名無しさん@着ぐるみすと" ? attr("xml:lang", "ja") : attr("rdf:datatype", "&xsd;string", 0)), sanitize(name))
 				}
 
 				if(trip != "") {
-					creator = creator element("dcterms:identifier", "rdf:datatype=\"&xsd;string\"", trip)
+					creator = creator elem("dcterms:identifier", attr("rdf:datatype", "&xsd;string", 0), trip)
 				}
 
-				post = post element("dcterms:creator", "rdf:parseType=\"Resource\"", creator, 0)
+				post = post elem("dcterms:creator", attr("rdf:parseType", "Resource"), creator, 0)
 			}
 
 			if(date != "") {
-				post = post element("sioc:delivered_at", "rdf:datatype=\"&dcterms;W3CDTF\"", date)
-				post = post element("sioc:content", "xml:lang=\"ja\"", str_sanitize(content, "\t\n"))
+				post = post elem("sioc:delivered_at", attr("rdf:datatype", "&dcterms;W3CDTF", 0), date)
+				post = post elem("sioc:content", attr("xml:lang", "ja"), sanitize(content))
 
 				refs = ""
 				while(match(content, />>([1-9][0-9]?[0-9]?|1000)/)) {
@@ -60,18 +61,18 @@ awkScript=$(
 
 					if(!index(refs, "#" ref_number ",") && index(content, "-") != 1) {
 						refs = refs "#" ref_number ","
-						post = post element("dcterms:relation", "rdf:resource=\"&post;" ref_number "\"")
+						post = post elem("dcterms:relation", attr("rdf:resource", "&post;" ref_number, 0))
 					}
 				}
 
-				post = post element("schema:creativeWorkStatus", "xml:lang=\"en\"", "Published")
+				post = post elem("schema:creativeWorkStatus", attr("xml:lang", "en"), "Published")
 			} else {
-				post = post element("schema:creativeWorkStatus", "xml:lang=\"en\"", "Deleted")
+				post = post elem("schema:creativeWorkStatus", attr("xml:lang", "en"), "Deleted")
 			}
 
-			post = post element("sioc:has_container", "rdf:resource=\"&board;\"")
+			post = post elem("sioc:has_container", attr("rdf:resource", "&board;", 0))
 
-			printf("%s", element("types:BoardPost", "rdf:about=\"&post;" number "\"", post, 0))
+			printf("%s", elem("types:BoardPost", attr("rdf:about", "&post;" number, 0), post, 0))
 		}
 	}
 	__EOF__
@@ -104,5 +105,4 @@ template=$(
 	__EOF__
 )
 
-str_replace 'x' "${template}" '<!-- !CONTENT! -->' "$(awk -- "${awkScript}" "${1}" "${2}")"
-printf '%s' "${x}" | xmlstarlet fo -t -
+str_replace - "${template}" '<!-- !CONTENT! -->' "$(awk -- "${awkScript}" "${1}" "${2}")" | xmlstarlet fo -t -
